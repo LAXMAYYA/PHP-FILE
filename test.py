c4f557fc869a8f7dea00f4c877a99c01252f83b5
LAXMAYYA <124341508+LAXMAYYA@users.noreply.github.com>
2023-07-02 20:42:50 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import requests
-class TestATGWebsite(unittest.TestCase):
-    def test_website_loading(self):
-        response = requests.get('https://atg.world')
-        self.assertEqual(response.status_code, 200)
-if __name__ == '__main__':
-    unittest.main()
